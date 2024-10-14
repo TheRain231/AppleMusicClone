a53fc80d7133aabc78516749d296c16f83f95d52
Андрей Степанов <therain3164@yandex.ru>
2024-10-14 12:37:48 +0300

    Cleaned header 2

--- Apple Music/View/CustomHeader.swift
+++ Apple Music/View/CustomHeader.swift
@@ -7,12 +7,48 @@
 
 import SwiftUI
 
-struct CustomHeader: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct CustomHeader {
+    static func header() -> some View {
+        HStack{
+            Text("Home")
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+            
+            Spacer()
+            
+            Button {
+                
+            } label: {
+                Image(systemName: "person.circle.fill")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 30, height: 30)
+                    .clipShape(.circle)
+            }
+        }
+        .foregroundStyle(.primary)
+        .padding(15)
+        //здеась еще visualEffect на увелечение
     }
-}
-
-#Preview {
-    CustomHeader()
+    
+    static func overlay(_ scrollPos: CGFloat) -> some View {
+        VStack{
+            ZStack{
+                Rectangle()
+                    .fill(.ultraThickMaterial)
+                    .fill(Color(uiColor: UIColor.systemBackground).opacity(scrollPos > 40 ? 0 : 1))
+                    .animation(.easeInOut, value: scrollPos > 40)
+                    .ignoresSafeArea(edges: .top)
+                    .frame(height: 25)
+                
+                
+                Text("Home")
+                    .fontWeight(.semibold)
+                    .offset(y: -5)
+                    .opacity(scrollPos >= 30 ? 1 : 0)
+            }
+            .offset(y: scrollPos > 1 ? min(-10 + scrollPos / 3, 0) : -10)
+            Spacer()
+        }
+    }
 }
--- Apple Music/View/HomeView.swift
+++ Apple Music/View/HomeView.swift
@@ -24,8 +24,7 @@ struct HomeView: View {
             
             .scrollTargetBehavior(.viewAligned)
             .safeAreaInset(edge: .top) {
-                header()
-                    
+                CustomHeader.header() 
             }
             .scrollTargetLayout()
         }
@@ -36,52 +35,10 @@ struct HomeView: View {
             withAnimation(.easeInOut(duration: 0.1)) {
                 scrollPos = newValue
             }
-            print(scrollPos)
         }
         .overlay {
-            VStack{
-                ZStack{
-                    Rectangle()
-                        .fill(.ultraThickMaterial)
-                        .fill(Color(uiColor: UIColor.systemBackground).opacity(scrollPos > 40 ? 0 : 1))
-                        .animation(.easeInOut, value: scrollPos > 40)
-                        .ignoresSafeArea(edges: .top)
-                        .frame(height: 25)
-                    
-                    
-                    Text("Home")
-                        .fontWeight(.semibold)
-                        .offset(y: -5)
-                        .opacity(scrollPos >= 30 ? 1 : 0)
-                }
-                .offset(y: scrollPos > 1 ? min(-10 + scrollPos / 3, 0) : -10)
-                Spacer()
-            }
+            CustomHeader.overlay(scrollPos)
         }
-    }
-    
-    @ViewBuilder
-    func header() -> some View {
-        HStack{
-            Text("Home")
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-            
-            Spacer()
-            
-            Button {
-                
-            } label: {
-                Image(systemName: "person.circle.fill")
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: 30, height: 30)
-                    .clipShape(.circle)
-            }
-        }
-        .foregroundStyle(.primary)
-        .padding(15)
-        //здеась еще visualEffect на увелечение
     }
     
     @ViewBuilder
