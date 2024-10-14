cfdebab1ab330503f18c4efa213f0f2122c05a77
Андрей Степанов <therain3164@yandex.ru>
2024-10-14 12:37:30 +0300

    Cleaned header

---
+++ Apple Music/View/CustomHeader.swift
@@ -0,0 +1,18 @@
+//
+//  CustomHeader.swift
+//  Apple Music
+//
+//  Created by Андрей Степанов on 14.10.2024.
+//
+
+import SwiftUI
+
+struct CustomHeader: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomHeader()
+}
